6611173857ebd30cd91ecf76a43aa5d3a54fe981
Sagar Gnawali <bamghawahadi@gmail.com>
2020-09-03 01:25:06 +0530

    calculator

---
+++ script/jscode.js
@@ -0,0 +1,28 @@
+//to add various expression enter by the user
+const add=(num)=> 
+{
+    document.form.text.value=document.form.text.value + num
+};
+
+//to evaluate user entered expression
+const equal= ()=>
+{
+    const data = document.form.text.value;
+    if(data)
+    {
+        document.form.text.value=eval(data);
+    }
+    else
+    {
+        document.form.text.value="error";
+    }
+
+
+}
+//to empty the input field
+empty = ()=> document.form.text.value="";
+function backward()    
+{
+    const data=document.form.text.value;
+    document.form.text.value=data.substring(0,data.length-1);
+}
